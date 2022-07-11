f30a3f9f7ace3587f7d17f5d7ee280ad69393f7c
kris164 <kris164@wp.pl>
2022-07-11 12:41:48 +0200

    Rejstracja przewoźnika

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,9 +9,11 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
-        public string Username { get; set; }
+        public string UsernameDto { get; set; }
         [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
+
+    
     }
 }
